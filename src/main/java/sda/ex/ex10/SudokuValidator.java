d646119370d857861c4e4c0a2771d0f085a390a7
katarzyna.musiol <katarzyna.musiol@yolt.com>
2021-08-29 11:54:37 +0200

    Sudoku

--- src/main/java/sda/ex/ex10/SudokuValidator.java
+++ src/main/java/sda/ex/ex10/SudokuValidator.java
@@ -23,6 +23,21 @@ public class SudokuValidator {
         cols = generateSets();
         sqrs = generateSets();
 
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int current = grid[i][j];
+
+                boolean addedToRow = rows.get(i).add(current);
+                boolean addedToColumn = cols.get(j).add(current);
+                boolean addedToSquare = sqrs.get(3 * (i / 3) + (j / 3)).add(current);
+
+                if (!(addedToRow && addedToColumn && addedToSquare)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     private List<Set<Integer>> generateSets() {
@@ -31,9 +46,6 @@ public class SudokuValidator {
             list.add(new HashSet<>());
         }
         return list;
-        /*return Stream.generate(HashSet::new)
-                .limit(9)
-                .collect(Collectors.toList())*/
     }
 
 }
